cafc47cba7684eb3101a964b181dfd94fffeedbf
chs1234 <chs4413@gmail.com>
2019-11-11 23:59:51 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,50 @@ import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
+import CircularProgress from '@material-ui/core/CircularProgress'
+import { withStyles } from '@material-ui/core/styles'
  
+/*
+
+  1) constructor()
+
+  2) componentWillMount()
+
+  3) render()
+
+  4) componentDidMount()
+
+*/
+
+/*
+
+  props of state => shouldComponentUpdate()
+
+*/
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing.unit *2
+  }
+});
+
 class App extends Component {
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then(res => this.setState({customers: res}))
       .catch(err => console.log(err));
@@ -27,10 +63,16 @@ class App extends Component {
     return body;
   }
 
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1})
+  }
+
   render() {
+    const { classes } = this.props;
     return (
-      <Paper>
-        <Table>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
           <TableHead>
             <TableRow>
               <TableCell>번호</TableCell>
@@ -44,7 +86,13 @@ class App extends Component {
           <TableBody>
           {this.state.customers ? this.state.customers.map(c => {
               return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthDay={c.birthDay} gender={c.gender} job={c.job} />)
-            }) : ""}
+            }) : 
+            <TableRow>
+              <TableCell colSpan="6" align="center">
+                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+              </TableCell>
+            </TableRow>
+            }
           </TableBody>
         </Table>
       </Paper>
@@ -52,4 +100,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
